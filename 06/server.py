152817ebcecd4545321b8e74e193739efc4afaf8
LeoQay <reliativism@yandex.ru>
2022-05-03 22:01:10 +0300

    06 init server

--- 06/server.py
+++ 06/server.py
@@ -1,38 +1,72 @@
-import multiprocessing
+import threading
 import socket
-import time
+import sys
+import argparse
+import urllib
 
 
-def server():
+def worker(num: int, top_k: int, info, locks: list[threading.Semaphore]):
+    while True:
+        locks[num].acquire()
+        if info[num]['end']:
+            return
+
+        info[num]['conn'].close()
+
+
+def master(workers_amount: int, top_k: int):
     sock = socket.socket()
-    sock.bind(("", 15000))
-    sock.listen(5)
+    sock.bind(('', 9090))
+    sock.listen(1)
+
+    info = [
+        {'free': True, 'stop': False, 'url': None, 'conn': None}
+        for _ in range(workers_amount)
+    ]
+
+    locks = [
+        threading.Semaphore()
+        for _ in range(workers_amount)
+    ]
+
+    for i in range(workers_amount):
+        locks[i].acquire()
+
+    threads = [
+        threading.Thread(target=worker, args=(i, top_k, info, locks),
+                         daemon=True)
+        for i in range(workers_amount)
+    ]
+
+    for th in threads:
+        th.start()
 
     while True:
-        client, addr = sock.accept()
-        print("server: conn from", addr)
+        conn, address = sock.accept()
+        url = conn.recv(1024)
+        free_num = None
+        while free_num is None:
+            for i in range(workers_amount):
+                if info[i]['free']:
+                    free_num = i
+                    break
+        info[free_num]['free'] = False
+        info[free_num]['url'] = url
+        info[free_num]['conn'] = conn
+        locks[free_num].release()
 
-        data = client.recv(4096)
-        print("server:", data)
-        client.send(data.upper())
-        break
+
+def get_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-w', type=int, default=10)
+    parser.add_argument('-k', type=int, default=7)
+    return parser.parse_args(args=sys.argv[1:]).__dict__
 
 
-def client():
-    sock = socket.socket()
-    sock.connect(("", 15000))
-    sock.sendall(b"qwerty")
-    data = sock.recv(1024)
-    print("client:", data)
+def main():
+    args = get_args()
+    master(args['w'], args['k'])
 
 
 if __name__ == "__main__":
-    p1 = multiprocessing.Process(target=server)
-    p2 = multiprocessing.Process(target=client)
-
-    p1.start()
-    time.sleep(1)
-    p2.start()
-
-    p1.join()
-    p2.join()
+    main()
