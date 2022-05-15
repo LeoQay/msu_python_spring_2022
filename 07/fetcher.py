6d743082b9366f3cddf03b6639551646127d7916
LeoQay <reliativism@yandex.ru>
2022-05-16 01:18:52 +0300

    07 init

--- 07/fetcher.py
+++ 07/fetcher.py
@@ -8,10 +8,9 @@ from collections import Counter
 
 
 top_k = 5
-results = {}
 
 
-async def fetch_url(session, url):
+async def fetch_url(session, url, results):
     try:
         async with session.get(url) as response:
             data = await response.text()
@@ -22,20 +21,28 @@ async def fetch_url(session, url):
         results[url] = str(e)
 
 
-async def worker(work_queue):
+async def worker(work_queue, results):
     async with aiohttp.ClientSession() as session:
         while not work_queue.empty():
             url = await work_queue.get()
-            await fetch_url(session, url)
+            await fetch_url(session, url, results)
 
 
 async def main(args):
+    results = {}
     work_queue = asyncio.Queue()
-    with open(args['urls'], 'r') as file:
-        for line in file:
-            await work_queue.put(line.strip())
-    tasks = [asyncio.create_task(worker(work_queue)) for _ in range(args['c'])]
+    try:
+        with open(args['urls'], 'r') as fp:
+            for line in fp:
+                await work_queue.put(line.strip())
+    except BaseException:
+        return {}
+    tasks = [
+        asyncio.create_task(worker(work_queue, results))
+        for _ in range(args['c'])
+    ]
     await asyncio.gather(*tasks)
+    return results
 
 
 def get_args(argv):
@@ -46,6 +53,6 @@ def get_args(argv):
 
 
 if __name__ == '__main__':
-    asyncio.run(main(get_args(sys.argv)))
+    result = asyncio.run(main(get_args(sys.argv)))
     with open('result.txt', 'w') as file:
-        print(json.dumps(results, indent=4), file=file)
+        print(json.dumps(result, indent=4), file=file)
---
+++ 07/test_fetcher.py
@@ -0,0 +1,34 @@
+import unittest
+import fetcher
+import asyncio
+
+
+class TestFetcher(unittest.TestCase):
+    def test_1(self):
+        args = {'c': 10, 'urls': 'files/not_existed.txt'}
+        result = asyncio.run(fetcher.main(args))
+        self.assertEqual(len(result), 0)
+
+    def test_2(self):
+        args = {'c': 5, 'urls': 'files/for_test_2.txt'}
+        result = asyncio.run(fetcher.main(args))
+        self.assertEqual(len(result), 0)
+
+    def test_3(self):
+        args = {'c': 15, 'urls': 'files/for_test_3.txt'}
+        result = asyncio.run(fetcher.main(args))
+        self.assertEqual(len(result), 1)
+        res = {
+            "https://web.archive.org/web/20210729085751/": {
+                "of": 19,
+                "icon": 15,
+                "An": 14,
+                "a": 13,
+                "illustration": 13
+            }
+        }
+        self.assertEqual(result, res)
+
+
+if __name__ == "__main__":
+    unittest.main()
