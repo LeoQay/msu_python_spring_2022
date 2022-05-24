2b428a16b8e60b77779b14f9405206f4f532410d
LeoQay <reliativism@yandex.ru>
2022-05-24 14:02:43 +0300

    10 update

--- 10/compar.py
+++ 10/compar.py
@@ -6,7 +6,55 @@ lib = ffi.dlopen('/home/leonid/PycharmProjects/msu_python_spring_2022/10/mullib.
 ffi.cdef('''void mul(int * a, int * b, int * res, int n, int m, int k);''')
 
 
-class Matrix:
+class PyMatrix:
+    def __init__(self, py_obj=None):
+        if py_obj is None or len(py_obj) == 0 or len(py_obj[0]) == 0:
+            self.shape = (0, 0)
+            self.arr = None
+        else:
+            self.shape = (len(py_obj), len(py_obj[0]))
+            for line in py_obj:
+                if len(line) != self.shape[1]:
+                    raise ValueError('Wrong number of columns')
+            self.arr = py_obj
+
+    def __mul__(self, other):
+        if self.shape[1] != other.shape[0]:
+            raise ValueError('Incompatible shapes')
+        if self.shape[0] == 0 or other.shape[0] == 0:
+            return PyMatrix()
+        result = PyMatrix.fill((self.shape[0], other.shape[1]))
+        PyMatrix.simple_mul(self, other, result)
+        return result
+
+    def __str__(self):
+        return str(self.arr)
+
+    @staticmethod
+    def simple_mul(first, second, result):
+        n_ax = first.shape[0]
+        m_ax = first.shape[1]
+        k_ax = second.shape[1]
+        first_arr = first.arr
+        second_arr = second.arr
+        res_arr = result.arr
+        for ind in range(n_ax):
+            for jnd in range(k_ax):
+                summa = 0
+                for knd in range(m_ax):
+                    summa += first_arr[ind][knd] * second_arr[knd][jnd]
+                res_arr[ind][jnd] = summa
+
+    @staticmethod
+    def fill(shape, value=0):
+        if type(value) != int:
+            raise TypeError('Value must be int')
+        if shape[0] == 0 or shape[1] == 0:
+            shape = (0, 0)
+        return PyMatrix([[value for _ in range(shape[1])] for _ in range(shape[0])])
+
+
+class CMatrix:
     def __init__(self, py_obj=None):
         self.shape = (0, 0)
         if py_obj is None or len(py_obj) == 0 or len(py_obj[0]) == 0:
@@ -28,8 +76,8 @@ class Matrix:
         if self.shape[1] != other.shape[0]:
             raise ValueError('Incompatible shapes')
         if self.shape[0] == 0 or other.shape[0] == 0:
-            return Matrix()
-        result = Matrix.fill((self.shape[0], other.shape[1]))
+            return CMatrix()
+        result = CMatrix.fill((self.shape[0], other.shape[1]))
         lib.mul(self.arr, other.arr, result.arr, self.shape[0], self.shape[1], other.shape[1])
         return result
 
@@ -45,11 +93,11 @@ class Matrix:
             raise TypeError('Value must be int')
         if shape[0] == 0 or shape[1] == 0:
             shape = (0, 0)
-        return Matrix([[value for _ in range(shape[1])] for _ in range(shape[0])])
+        return CMatrix([[value for _ in range(shape[1])] for _ in range(shape[0])])
 
 
 if __name__ == "__main__":
-    a = Matrix([[1, 2], [3, 4]])
+    a = PyMatrix([[1, 2], [3, 4]])
     b = a * a
     print(b)
 
