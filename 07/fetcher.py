8bdc0b5e07cabce338d04ed2d14795a8c4c98a9b
LeoQay <reliativism@yandex.ru>
2022-05-29 01:22:46 +0300

    07 fixes

--- 07/fetcher.py
+++ 07/fetcher.py
@@ -1,45 +1,53 @@
 import sys
 import asyncio
 import argparse
-import json
 from collections import Counter
 import aiohttp
 from bs4 import BeautifulSoup
 
 
-async def fetch_url(session, url, results):
+async def get_url(file_name):
+    with open(file_name, 'r', encoding='utf-8') as urls:
+        for url in urls:
+            yield url.strip()
+
+
+async def fetch_url(session, url):
     try:
         async with session.get(url) as response:
             data = await response.text()
-            soup = BeautifulSoup(data, features='html.parser')
-            stat = Counter(soup.get_text().split()).most_common(5)
-            results[url] = {pair[0]: pair[1] for pair in stat}
-    except BaseException as err:
-        results[url] = str(err)
+    except aiohttp.ClientHttpProxyError:
+        pass
+    except aiohttp.ClientConnectionError:
+        pass
+    except aiohttp.ClientConnectorError:
+        pass
+    soup = BeautifulSoup(data, features='html.parser')
+    return Counter(soup.get_text().split()).most_common(5)
 
 
-async def worker(work_queue, results):
+async def worker(lock, urls):
     async with aiohttp.ClientSession() as session:
-        while not work_queue.empty():
-            url = await work_queue.get()
-            await fetch_url(session, url, results)
+        while True:
+            try:
+                async with lock:
+                    url = await anext(urls)
+                stat = await fetch_url(session, url)
+                print(stat)
+            except StopAsyncIteration:
+                return
+            except ...:
+                pass
 
 
 async def main(args):
-    results = {}
-    work_queue = asyncio.Queue()
-    try:
-        with open(args['urls'], 'r', encoding='utf-8') as urls:
-            for line in urls:
-                await work_queue.put(line.strip())
-    except BaseException:
-        return {}
+    urls = get_url(args['urls'])
+    lock = asyncio.Lock()
     tasks = [
-        asyncio.create_task(worker(work_queue, results))
+        asyncio.create_task(worker(lock, urls))
         for _ in range(args['c'])
     ]
     await asyncio.gather(*tasks)
-    return results
 
 
 def get_args(argv):
@@ -50,6 +58,4 @@ def get_args(argv):
 
 
 if __name__ == '__main__':
-    result = asyncio.run(main(get_args(sys.argv)))
-    with open('result.txt', 'w', encoding='utf-8') as file:
-        print(json.dumps(result, indent=4), file=file)
+    asyncio.run(main(get_args(sys.argv)))
